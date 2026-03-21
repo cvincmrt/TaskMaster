fe4570db438bc3857faeef7c64ed034cc3416e5b
cvincmrt <cvincmrt@gmail.com>
2026-03-21 17:11:12 +0100

    pridal som class Task

--- src/Task.php
+++ src/Task.php
@@ -4,15 +4,45 @@ namespace App;
 
 abstract class Task
 {
+    protected ?int $id = null;
+    protected ?int $projectId = null;
+    protected ?int $userId = null;
     protected string $title;
-    protected string $status;
-    protected int $priority;
+    protected string $type; // todo, doing, done, default=todo
+    protected string $status; // bug, feature
+    protected int $priority; // default=1
 
-    public function __construct($title, $status, $priority){
+    public function __construct(int $projectId, int $userId, string $title, string $type = "todo", string $status, int $priority = 1){
+        $this->projectId = $projectId;
+        $this->userId = $userId;
         $this->title = $title;
+        $this->type = $type;
         $this->status = $status;
         $this->priority = $priority;
     }
 
+    abstract public function getCalculatedPriority() :int;
+
+    // Getery
+
+    public function getTitle() :string
+    {
+        return $this->title;
+    }
+
+    public function getType() :string
+    {
+        return $this->type;
+    }
+
+    public function getStatus() :string
+    {
+        return $this->status;
+    }
+
+    public function getPriority() :int
+    {
+        return $this->priority;
+    }
 
 }
